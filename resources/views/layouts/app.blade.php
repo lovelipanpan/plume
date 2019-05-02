e5f68044e72385a559f8482bf7c83687bd29751d
yangyu <13203836651@163.com>
2019-05-02 16:56:53 +0000

    准备工作做完，明天写页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'plume') - dream fly 梦想飞扬的地方</title>
+  <title>@yield('title', 'plume') - 嫁给我好么？</title>
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
